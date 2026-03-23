8a1c208490f4bdd7e589c8b723a02d0a3db085eb
siddhant <siddhant3103@gmail.com>
2026-03-23 22:11:54 +0530

    configured vite.config.js, to tell vercel to consider privacy.html also, before it was bundling only index.html is dist folder

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+      },
+    },
   },
   server: {
     port: 3000,
